8b95b89a7e369879b6afbe2efc16cb05cef79612
7Bssk7 <arhip.fin3@gmail.com>
2025-09-21 14:33:09 -0700

    Update

--- 210-lab-12.cpp
+++ 210-lab-12.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
+#include <algorithm>
+#include <numeric>
+#include <array>
+#include <fstream>
 using namespace std;
 
+const int SIZE = 40;
+
 int main(){
+    ifstream inputFile("grades.txt");
+    array<int, SIZE> grades;
+    int temp, i = 0;
+
+    if(inputFile.is_open()){
+        while(inputFile >> temp){
+            grades[i] = temp;
+            ++i;
+        }
+    }
+    else{
+        cout << "Error, file is not opening "<< endl;
+    }
+
+
 
     return 0;
 }
